50c519a18f575747e5b05eb4952f69263717fdf8
LeeTaeSuk <twohugerock@gmail.com>
2024-10-11 00:24:16 +0900

    더미 생성 쿼리 추가컬럼 생성

--- src/main/java/com/example/vitalic_back/repository/BatchJobSeqRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.vitalic_back.repository;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BatchJobSeqRepository extends JpaRepository<BatchJobSeq, Long> {
-}
